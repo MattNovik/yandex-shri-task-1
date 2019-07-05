862c979b64591a61248ee3a7e755b078e3bde3d1
AlexKale <alex-kale@hotmail.com>
2019-07-06 02:19:04 +0300

    update logic

--- build/script.js
+++ build/script.js
@@ -1,10 +1,11 @@
 document.body.addEventListener('click', (e) => {
   const onoffswitch = e.target.closest('.onoffswitch');
+  const accordionShort = e.target.closest('.e-accordion__short');
   if (onoffswitch) {
     document.querySelectorAll('.theme_color_project-default')
       .forEach(el => el.classList.toggle('theme_color_project-inverse'));
     onoffswitch.classList.toggle('onoffswitch_on');
-  } else if (e.target.closest('.e-accordion__short')) {
-    e.target.closest('.e-accordion__short').nextElementSibling.classList.toggle('e-accordion__more_show');
+  } else if (accordionShort) {
+    accordionShort.nextElementSibling.classList.toggle('e-accordion__more_show');
   }
 });
